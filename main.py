f3ca55e6ef7356d898b897df0411635743f4051c
noamfuss <noamfuss@gmail.com>
2025-02-04 23:33:10 +0200

    Switch to waitress

--- main.py
+++ main.py
@@ -4,67 +4,67 @@ from datetime import datetime
 import threading
 import time
 import re
+from waitress import serve
+from os import getenv
 
 app = Flask(__name__)
 
 # Global variable to store discovered instances
 discovered_instances = []
 
-def get_glances_instances():
+def get_traefik_routers():
     """
-    Scan Docker containers for Traefik router rules related to Glances
+    Scan Docker containers for any Traefik router rules
     """
     client = docker.from_env()
     instances = []
     
     try:
         containers = client.containers.list()
-        print(f"{containers = }")
         for container in containers:
             labels = container.labels
-            # Look for the specific Traefik router rule
-            rule_key = 'traefik.http.routers.glances.rule'
-            print(f"{labels = }")
-            match = re.search(r'traefik\.http\.routers\..*.rule', labels)
-            if rule_key in labels:
-                instance = {
-                    'container_id': container.short_id,
-                    'name': container.name,
-                    'rule': labels[rule_key],
-                    'status': container.status,
-                    'created': container.attrs['Created'],
-                }
-                instances.append(instance)
+            
+            # Find all Traefik router rules using regex pattern
+            router_pattern = re.compile(r'traefik\.http\.routers\.(.+)\.rule')
+            
+            for label, value in labels.items():
+                match = router_pattern.match(label)
+                if match:
+                    router_name = match.group(1)
+                    instance = {
+                        'container_id': container.short_id,
+                        'name': container.name,
+                        'router_name': router_name,
+                        'rule': value,
+                        'status': container.status,
+                        'created': container.attrs['Created'],
+                    }
+                    instances.append(instance)
+    
     except docker.errors.APIError as e:
         print(f"Error connecting to Docker: {e}")
     except Exception as e:
         print(f"Unexpected error: {e}")
-
-    print(f"{instances = }")
+        
     return instances
-
 
 def background_scanner():
     """
-    Background task to periodically scan for Glances instances
+    Background task to periodically scan for Traefik routers
     """
     global discovered_instances
     while True:
-        discovered_instances = get_glances_instances()
+        discovered_instances = get_traefik_routers()
         time.sleep(30)  # Scan every 30 seconds
 
-
-# Create templates directory and add HTML template
 @app.route('/')
 def home():
     return render_template('index.html', instances=discovered_instances)
 
-
 if __name__ == '__main__':
-    
     # Start background scanner thread
     scanner_thread = threading.Thread(target=background_scanner, daemon=True)
     scanner_thread.start()
     
     # Run Flask app
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    serve(app, host=getenv("HOST", '0.0.0.0'), port=getenv("PORT", 5000))
